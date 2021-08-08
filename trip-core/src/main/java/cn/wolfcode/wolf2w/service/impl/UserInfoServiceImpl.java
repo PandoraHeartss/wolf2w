08291d8da40b98647b6088a8d257e7c2cd4aabfc
PandoraHearts <1512836768@qq.com>
2021-08-08 16:22:18 +0800

    完成注册功能

--- trip-core/src/main/java/cn/wolfcode/wolf2w/service/impl/UserInfoServiceImpl.java
+++ trip-core/src/main/java/cn/wolfcode/wolf2w/service/impl/UserInfoServiceImpl.java
@@ -5,6 +5,7 @@ import cn.wolfcode.wolf2w.mapper.UserInfoMapper;
 import cn.wolfcode.wolf2w.service.IUserInfoService;
 import cn.wolfcode.wolf2w.redis.IUserInfoRedisService;
 
+import cn.wolfcode.wolf2w.util.AssertUtil;
 import cn.wolfcode.wolf2w.util.Consts;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -58,7 +59,7 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
     @Override
     public void sendVerifyCode(String phone) {
         //用UUID创建短信验证码
-        String uuid = UUID.randomUUID().toString().replace("-", "");
+        String uuid = UUID.randomUUID().toString().replace("-", "").substring(0, 4);
         //拼接短信
         StringBuffer sb = new StringBuffer();
         String letter = sb.append("您的短信验证码是：").append(uuid).append(",请在").append(Consts.VERIFY_CODE_VAI_TIME).append("分钟内登录！").toString();
@@ -66,5 +67,54 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
         System.out.println(letter);
         //调用redis的业务方法，缓存短信到redis中去
         userInfoRedisService.setVerifyCode(phone, uuid);
+    }
+
+
+    /*
+     * @Description: 注册功能的业务逻辑的实现
+     * @param: phone 手机号码
+     * @param: nickname 昵称
+     * @param: password 密码
+     * @param: rpassword 确认密码
+     * @param: verifyCode 验证码
+     * @return void
+     * @author PandoraHearts
+     * @date 2021/8/8 15:11
+     */
+    @Override
+    public void regist(String phone, String nickname, String password, String rpassword, String verifyCode) {
+
+        //判断参数是否为null
+        //断言操作，底层跟StringUtils一样判空，但一般不用项目框架提供的，而是自己写一个这个工具类AssertUtil
+        AssertUtil.hasLength(phone, "手机号码不能为空");
+        AssertUtil.hasLength(nickname, "昵称不能为空");
+        AssertUtil.hasLength(password, "密码不能为空");
+        AssertUtil.hasLength(rpassword, "确认密码不能为空");
+        AssertUtil.hasLength(verifyCode, "验证码不能为空");
+
+        //判断2次输入密码是否一致
+        AssertUtil.isEquals(password, rpassword, "两次输入密码不一样");
+
+        //判断手机格式是否正确 @TODO java 正则
+
+        //判断手机号码是否唯一
+        if (this.checkPhone(phone)) {
+            throw new RuntimeException("该手机号已被注册");
+        }
+
+        //判断验证码是否唯一
+        String code = userInfoRedisService.getVerifyCode(phone);
+        if (!verifyCode.equalsIgnoreCase(code)) {
+            throw new RuntimeException("该验证码输入错误或失效");
+        }
+
+        //注册
+        UserInfo userInfo = new UserInfo();
+        userInfo.setHeadImgUrl("/images/default.jpg");
+        userInfo.setPhone(phone);
+        userInfo.setNickname(nickname);
+        userInfo.setPassword(password);
+        userInfo.setState(UserInfo.STATE_NORMAL);
+        super.save(userInfo);
     }
 }
--- trip-website-api/src/main/java/cn/wolfcode/wolf2w/controller/UserInfoController.java
+++ trip-website-api/src/main/java/cn/wolfcode/wolf2w/controller/UserInfoController.java
@@ -1,11 +1,9 @@
 package cn.wolfcode.wolf2w.controller;
 
 import cn.wolfcode.wolf2w.service.IUserInfoService;
+import cn.wolfcode.wolf2w.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
@@ -24,7 +22,7 @@ public class UserInfoController {
 
     /*
      * @Description: 询输入的手机号是否已被注册
-     * @param: null
+     * @param: phone 手机号码
      * @return true为查找到该手机号  false为查找不到该手机号
      * @author PandoraHearts
      * @date 2021/8/6 16:47
@@ -33,6 +31,47 @@ public class UserInfoController {
     public Boolean checkPhone(String phone) {
         Boolean ret = userInfoService.checkPhone(phone);
         return ret;
+    }
+
+
+    /*
+     * @Description:发送短信验证码
+     * @param: phone 手机号码
+     * @return 返回json格式的信息
+     * @author PandoraHearts
+     * @date 2021/8/8 10:26
+     */
+    @GetMapping("/sendVerifyCode")
+    public JsonResult sendVerifyCode(String phone) {
+        //调用短信发送的业务方法
+        userInfoService.sendVerifyCode(phone);
+        return JsonResult.success();
+    }
+
+
+    /*
+     * @Description: 注册功能
+     * @param: phone 手机号码
+     * @param: nickname 昵称
+     * @param: password 密码
+     * @param: rpassword 确认密码
+     * @param: verifyCode 验证码
+     * @return java.lang.Object
+     * @author PandoraHearts
+     * @date 2021/8/8 15:01
+     */
+    @PostMapping("/regist")
+    public Object regist(String phone, String nickname, String password, String rpassword, String verifyCode) {
+
+        try {
+            //调用注册的业务方法
+            userInfoService.regist(phone, nickname, password, rpassword, verifyCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return JsonResult.error(JsonResult.CODE_ERROR, JsonResult.MSG_ERROR, null);
+        }
+
+        return JsonResult.success();
     }
 
 
